145cfc7e448c0b1db31d7266fd261404b321bf2a
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-04-12 11:35:42 -0500

    Primer commit

---
+++ src/main/java/com/losmascapitos/mascapitosmod/Mascapitosmod.java
@@ -0,0 +1,8 @@
+package com.losmascapitos.mascapitosmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Mascapitosmod.MODID)
+public class Mascapitosmod {
+    public static final String MODID = "mascapitosmod";
+}
